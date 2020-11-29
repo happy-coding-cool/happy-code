c38e3bf824633a3aea9e86a4595349e0e2a592ff
pengzhenchen <pzsoftchen@163.com>
2020-11-29 18:12:07 +0800

    feat: 测试web组件

--- happy-code-samples/happy-code-starter-web-sample/src/main/java/cool/happycoding/web/sample/rest/ResultController.java
+++ happy-code-samples/happy-code-starter-web-sample/src/main/java/cool/happycoding/web/sample/rest/ResultController.java
@@ -1,6 +1,12 @@
 package cool.happycoding.web.sample.rest;
 
+import cn.hutool.core.date.DateUtil;
+import cool.happycoding.base.result.BaseResult;
+import cool.happycoding.web.sample.bean.SampleWeb;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 /**
  * description
@@ -9,5 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ResultController {
+
+
+    @GetMapping
+    public BaseResult<SampleWeb> get(String id){
+        return BaseResult.success(SampleWeb
+                .builder()
+                .id(id)
+                .addr("深圳")
+                .age(40)
+                .birth(DateUtil.parse("1980-10-21"))
+                .salary(BigDecimal.valueOf(1234567L))
+                .userName("深圳")
+                .build());
+    }
 
 }
