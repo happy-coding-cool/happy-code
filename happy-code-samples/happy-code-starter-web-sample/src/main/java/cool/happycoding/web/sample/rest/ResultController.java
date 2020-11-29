40667885021e330e36c2a6e7b546c570d0fc6f35
pengzhenchen <pzsoftchen@163.com>
2020-11-29 17:37:23 +0800

    feat: 添加web-sample模块用于web组件的Result的使用测试

---
+++ happy-code-samples/happy-code-starter-web-sample/src/main/java/cool/happycoding/web/sample/rest/ResultController.java
@@ -0,0 +1,13 @@
+package cool.happycoding.web.sample.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2020/11/29 5:36 下午
+ */
+@RestController
+public class ResultController {
+
+}
