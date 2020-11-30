24eac8661e863ac43ba0117153137fe4fd025703
pengzhenchen <pzsoftchen@163.com>
2020-11-30 13:47:37 +0800

    feat: 添加全局异常拦截处理类

---
+++ happy-code-starters/happy-code-starter-web/src/main/java/cool/happycoding/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package cool.happycoding.web.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * <p>
+ *     统一的异常拦截处理
+ * </p>
+ *
+ * @author lanlanhappy 2020/11/30 1:45 下午
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
