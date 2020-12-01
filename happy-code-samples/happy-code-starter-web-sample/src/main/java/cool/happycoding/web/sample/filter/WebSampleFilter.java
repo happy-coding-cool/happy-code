2148274baa167f6b17a890d2ca823b4d91d2d416
pengzhenchen <pzsoftchen@163.com>
2020-12-01 21:30:34 +0800

    test: 测试404/filter异常情形的统一处理

---
+++ happy-code-samples/happy-code-starter-web-sample/src/main/java/cool/happycoding/web/sample/filter/WebSampleFilter.java
@@ -0,0 +1,28 @@
+package cool.happycoding.web.sample.filter;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import static cool.happycoding.base.util.HappyCodeUtil.check;
+import static cool.happycoding.web.sample.WebStatus.PARAM_ILLEGAL;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2020/12/01 9:06 下午
+ */
+@Component
+@WebFilter("/exception")
+public class WebSampleFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        check(StrUtil.equalsAnyIgnoreCase(request.getParameter("id"), "999"), PARAM_ILLEGAL);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
