9fc7edf5d128dcf9c627e0fabbcb0b83f50a3a4f
pengzhenchen <pzsoftchen@163.com>
2020-12-03 18:12:33 +0800

    test: 测试validator组件的使用

--- happy-code-samples/happy-code-starter-validator-sample/src/main/java/cool/happycoding/validator/sample/rest/ValidatorController.java
+++ happy-code-samples/happy-code-starter-validator-sample/src/main/java/cool/happycoding/validator/sample/rest/ValidatorController.java
@@ -1,7 +1,9 @@
 package cool.happycoding.validator.sample.rest;
 
 import cool.happycoding.base.result.BaseResult;
+import cool.happycoding.validator.ValidatorEnhance;
 import cool.happycoding.validator.sample.bean.ValidateBean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +18,19 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("validate")
+@RequiredArgsConstructor
 public class ValidatorController {
+
+    private final ValidatorEnhance validatorEnhance;
 
     @PostMapping("val")
     public BaseResult<ValidateBean> validate(@Valid @RequestBody ValidateBean validateBean){
+        return BaseResult.success(validateBean);
+    }
+
+    @PostMapping("val-tool")
+    public BaseResult<ValidateBean> validateTool(@RequestBody ValidateBean validateBean){
+        validatorEnhance.valid(validateBean);
         return BaseResult.success(validateBean);
     }
 }
