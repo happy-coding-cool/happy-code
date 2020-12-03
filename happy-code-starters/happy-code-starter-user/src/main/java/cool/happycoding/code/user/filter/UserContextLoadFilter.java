0a5dfbd8c7cf20397f3677b8e98a000fd1bb331a
pengzhenchen <pzsoftchen@163.com>
2020-12-03 21:27:58 +0800

    feat: 添加UserContextLoadFilter

---
+++ happy-code-starters/happy-code-starter-user/src/main/java/cool/happycoding/code/user/filter/UserContextLoadFilter.java
@@ -0,0 +1,19 @@
+package cool.happycoding.code.user.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * <p>
+ *     加载 user context
+ * </p>
+ *
+ * @author lanlanhappy 2020/12/03 9:26 下午
+ */
+public class UserContextLoadFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
