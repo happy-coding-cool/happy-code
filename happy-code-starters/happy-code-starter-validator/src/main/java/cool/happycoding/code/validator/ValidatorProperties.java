3417ec674ae2e349077c4204a7cf4aa5c48ad70e
pengzhenchen <pzsoftchen@163.com>
2020-12-04 21:52:46 +0800

    refactor: 重构代码

--- happy-code-starters/happy-code-starter-validator/src/main/java/cool/happycoding/code/validator/ValidatorProperties.java
+++ happy-code-starters/happy-code-starter-validator/src/main/java/cool/happycoding/code/validator/ValidatorProperties.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @ConfigurationProperties(prefix = ValidatorProperties.HAPPY_BOOT_VALIDATE_PREFIX)
 public class ValidatorProperties {
 
-    public static final String HAPPY_BOOT_VALIDATE_PREFIX = "happy.boot.validator";
+    public static final String HAPPY_BOOT_VALIDATE_PREFIX = "happy.code.validator";
     /**
      * 是否采用快速失败模式，默认 false
      */
