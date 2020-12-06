0170ae23b99f1cb0da37443fe90efd3a81d71fa4
pengzhenchen <pzsoftchen@163.com>
2020-12-06 15:27:22 +0800

    test: 测试mybatis组件

--- happy-code-samples/happy-code-starter-mybatis-sample/src/main/java/cool/happycoding/code/mybatis/sample/domain/entity/User.java
+++ happy-code-samples/happy-code-starter-mybatis-sample/src/main/java/cool/happycoding/code/mybatis/sample/domain/entity/User.java
@@ -1,12 +1,36 @@
 package cool.happycoding.code.mybatis.sample.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import cool.happycoding.code.mybatis.base.BaseEntity;
+import lombok.Data;
+
+import java.math.BigDecimal;
 
 /**
  * description
  *
  * @author lanlanhappy 2020/12/06 2:26 下午
  */
+@Data
+@TableName("h2user")
 public class User extends BaseEntity {
 
+    @TableField("name")
+    private String name;
+
+    @TableField("age")
+    private Integer age;
+
+    @TableField("gender")
+    private String gender;
+
+    @TableField("addr")
+    private String addr;
+
+    @TableField("salary")
+    private BigDecimal salary;
+
+    @TableField("remark")
+    private String remark;
 }
--- happy-code-samples/happy-code-starter-mybatis-sample/src/main/java/cool/happycoding/code/mybatis/sample/domain/mapper/UserMapper.java
+++ happy-code-samples/happy-code-starter-mybatis-sample/src/main/java/cool/happycoding/code/mybatis/sample/domain/mapper/UserMapper.java
@@ -1,9 +1,15 @@
 package cool.happycoding.code.mybatis.sample.domain.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import cool.happycoding.code.mybatis.sample.domain.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * description
  *
  * @author lanlanhappy 2020/12/06 2:26 下午
  */
-public interface UserMapper {
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
 }
