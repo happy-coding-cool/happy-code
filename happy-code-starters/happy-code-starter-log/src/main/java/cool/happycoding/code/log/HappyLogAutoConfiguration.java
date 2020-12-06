1c3c36261a4c40227d058048559cbc63124b3446
pengzhenchen <pzsoftchen@163.com>
2020-12-06 11:34:43 +0800

    feat: 完善log组件

---
+++ happy-code-starters/happy-code-starter-log/src/main/java/cool/happycoding/code/log/HappyLogAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cool.happycoding.code.log;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2020/12/06 11:33 上午
+ */
+@Configuration
+public class HappyLogAutoConfiguration {
+}
