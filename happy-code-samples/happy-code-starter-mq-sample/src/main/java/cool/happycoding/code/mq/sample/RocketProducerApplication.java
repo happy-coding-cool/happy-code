7068cf1e53c83fdf0e4263dd52a9337355ad2047
pengzhenchen <pzsoftchen@163.com>
2020-12-12 22:25:34 +0800

    feat: 完善mq组件并测试

---
+++ happy-code-samples/happy-code-starter-mq-sample/src/main/java/cool/happycoding/code/mq/sample/RocketProducerApplication.java
@@ -0,0 +1,18 @@
+package cool.happycoding.code.mq.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2020/12/12 5:17 下午
+ */
+@SpringBootApplication
+public class RocketProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketProducerApplication.class, args);
+    }
+
+}
---
+++ happy-code-starters/happy-code-starter-mq/src/main/java/cool/happycoding/code/mq/HappyMqAutoConfiguration.java
@@ -0,0 +1,13 @@
+package cool.happycoding.code.mq;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2020/12/12 4:53 下午
+ */
+@Configuration
+public class HappyMqAutoConfiguration {
+
+}
