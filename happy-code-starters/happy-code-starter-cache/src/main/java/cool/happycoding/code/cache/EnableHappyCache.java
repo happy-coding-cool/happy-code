da6377300cb1d1566351f2b965cae89226e8b666
pengzhenchen <pzsoftchen@163.com>
2021-02-21 18:21:36 +0800

    fixed：修复注解定义

--- happy-code-starters/happy-code-starter-cache/src/main/java/cool/happycoding/code/cache/EnableHappyCache.java
+++ happy-code-starters/happy-code-starter-cache/src/main/java/cool/happycoding/code/cache/EnableHappyCache.java
@@ -1,9 +1,27 @@
 package cool.happycoding.code.cache;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
 /**
  * description
  *
  * @author lanlanhappy 2021/02/21 11:34 上午
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@EnableMethodCache(basePackages = {})
+@EnableCreateCacheAnnotation
 public @interface EnableHappyCache {
+
+    @AliasFor(
+            annotation = EnableMethodCache.class,
+            attribute = "basePackages"
+    )
+    String[] basePackages();
 }
