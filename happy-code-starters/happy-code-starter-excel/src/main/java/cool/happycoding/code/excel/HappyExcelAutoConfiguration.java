4904bc9bcbbc5855ecf3f93aaec6e800fc224d79
pengzhenchen <pzsoftchen@163.com>
2021-02-25 21:10:41 +0800

    feat：添加excel组件定义

---
+++ happy-code-starters/happy-code-starter-excel/src/main/java/cool/happycoding/code/excel/HappyExcelAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cool.happycoding.code.excel;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2021/02/25 9:05 下午
+ */
+@Configuration
+public class HappyExcelAutoConfiguration {
+}
