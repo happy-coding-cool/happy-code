a2cf0f5190b490677329ae96726e810a0a4f0669
pengzhenchen <pzsoftchen@163.com>
2021-02-25 21:10:52 +0800

    feat：添加store存储组件定义

---
+++ happy-code-starters/happy-code-starter-store/src/main/java/cool/happycoding/code/store/HappyStoreAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cool.happycoding.code.store;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2021/02/25 9:09 下午
+ */
+@Configuration
+public class HappyStoreAutoConfiguration {
+}
