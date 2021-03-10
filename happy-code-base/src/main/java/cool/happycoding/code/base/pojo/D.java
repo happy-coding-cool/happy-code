f80af42b8a58a0317ea988132e97a5092015672f
pengzhenchen <pzsoftchen@163.com>
2021-03-10 21:51:11 +0800

    refactor：代码优化

--- happy-code-base/src/main/java/cool/happycoding/code/base/pojo/D.java
+++ happy-code-base/src/main/java/cool/happycoding/code/base/pojo/D.java
@@ -1,11 +1,9 @@
 package cool.happycoding.code.base.pojo;
 
-import java.io.Serializable;
-
 /**
  * description
  *
  * @author lanlanhappy 2020/12/05 1:45 下午
  */
-public interface D extends Serializable {
+public interface D extends H {
 }
