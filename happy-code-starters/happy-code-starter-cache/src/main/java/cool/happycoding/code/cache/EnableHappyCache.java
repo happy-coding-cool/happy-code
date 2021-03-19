c2026bef724210919e2ab76d29804cb9c1854657
pengzhenchen <pzsoftchen@163.com>
2021-03-19 14:27:21 +0800

    fix：修复cache组件组合注解的bug

--- happy-code-starters/happy-code-starter-cache/src/main/java/cool/happycoding/code/cache/EnableHappyCache.java
+++ happy-code-starters/happy-code-starter-cache/src/main/java/cool/happycoding/code/cache/EnableHappyCache.java
@@ -2,6 +2,8 @@ package cool.happycoding.code.cache;
 
 import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.*;
@@ -24,4 +26,24 @@ public @interface EnableHappyCache {
             attribute = "basePackages"
     )
     String[] basePackages();
+
+    @AliasFor(
+            annotation = EnableMethodCache.class,
+            attribute = "proxyTargetClass"
+    )
+    boolean proxyTargetClass() default false;
+
+    @AliasFor(
+            annotation = EnableMethodCache.class,
+            attribute = "mode"
+    )
+    AdviceMode mode() default AdviceMode.PROXY;
+
+    @AliasFor(
+            annotation = EnableMethodCache.class,
+            attribute = "order"
+    )
+    int order() default Ordered.LOWEST_PRECEDENCE;
+
+
 }
