6040de7446cf8e907135bce839e299b0789890bc
pengzhenchen <pzsoftchen@163.com>
2021-04-28 19:42:14 +0800

    feat：完善lock组件

---
+++ happy-code-starters/happy-code-starter-lock/src/main/java/cool/happycoding/code/lock/HappyLockAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cool.happycoding.code.lock;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2021/04/28 7:40 下午
+ */
+@Configuration
+public class HappyLockAutoConfiguration {
+}
