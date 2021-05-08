b95bf57a8f8ce85bd536eef00a2648a531aa3712
pengzhenchen <pzsoftchen@163.com>
2021-05-08 17:42:44 +0800

    feat：完善字典翻译组件

---
+++ happy-code-starters/happy-code-starter-dict/src/main/java/cool/happycoding/code/dict/HappyDictAutoConfiguration.java
@@ -0,0 +1,14 @@
+package cool.happycoding.code.dict;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2021/05/08 5:42 下午
+ */
+@Configuration
+public class HappyDictAutoConfiguration {
+
+    
+}
