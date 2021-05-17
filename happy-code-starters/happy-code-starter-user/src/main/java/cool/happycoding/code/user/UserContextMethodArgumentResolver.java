f9bfa14325a70e1b6ac6a8be3e9d6a65e8731e91
pengzhenchen <pzsoftchen@163.com>
2021-05-17 10:40:45 +0800

    feat：代码优化

--- happy-code-starters/happy-code-starter-user/src/main/java/cool/happycoding/code/user/UserContextMethodArgumentResolver.java
+++ happy-code-starters/happy-code-starter-user/src/main/java/cool/happycoding/code/user/UserContextMethodArgumentResolver.java
@@ -1,6 +1,7 @@
 package cool.happycoding.code.user;
 
 import cn.hutool.core.bean.BeanUtil;
+import cool.happycoding.code.base.user.User;
 import cool.happycoding.code.user.context.UserContextHolder;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -17,11 +18,13 @@ public class UserContextMethodArgumentResolver implements HandlerMethodArgumentR
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(CurrentUser.class);
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && parameter.getParameterType().equals(User.class);
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         return BeanUtil.copyProperties(UserContextHolder.getUser(), parameter.getParameterType());
     }
 }
