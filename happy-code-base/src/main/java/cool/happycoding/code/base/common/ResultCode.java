66c2e28653ae4740388ce6f695c322bf7b926838
pengzhenchen <pzsoftchen@163.com>
2021-10-11 13:33:58 +0800

    feat: 添加支持国际化的配置

--- happy-code-base/src/main/java/cool/happycoding/code/base/common/ResultCode.java
+++ happy-code-base/src/main/java/cool/happycoding/code/base/common/ResultCode.java
@@ -1,5 +1,10 @@
 package cool.happycoding.code.base.common;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Objects;
+
 /**
  * <p>
  *     返回状态描述
@@ -29,4 +34,15 @@ public interface ResultCode {
      * @return message
      */
     String getMessage();
+
+    /**
+     * 支持国际化
+     * @param args format args
+     * @param messageSource messageSource
+     * @return message
+     */
+    default String getMessage(Object[] args, MessageSource messageSource){
+        return Objects.isNull(messageSource) ? getMessage()
+                : messageSource.getMessage(getCode(), args, LocaleContextHolder.getLocale());
+    }
 }
