17137dcc57478ca86e8266a2a44e66dcd6db163b
pengzhenchen <pzsoftchen@163.com>
2021-11-27 20:51:47 +0800

    feat: add webservice client

---
+++ happy-code-starters/happy-code-starter-ws-client/src/main/java/cool/happycoding/code/ws/client/HappyWsClientAutoConfiguration.java
@@ -0,0 +1,11 @@
+package cool.happycoding.code.ws.client;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2021/10/23 3:12 下午
+ */
+public class HappyWsClientAutoConfiguration {
+
+
+}
---
+++ happy-code-starters/happy-code-starter-ws-client/src/main/java/cool/happycoding/code/ws/client/annotation/EnableWsClients.java
@@ -0,0 +1,9 @@
+package cool.happycoding.code.ws.client.annotation;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2021/10/23 3:14 下午
+ */
+public @interface EnableWsClients {
+}
